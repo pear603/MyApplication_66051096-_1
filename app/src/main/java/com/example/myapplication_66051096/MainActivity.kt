5bcaf2d7f4a5b93d6de50d090fe89811a89b571f
pear603 <Sujareepear@gmail.com>
2025-08-04 23:46:34 +0700

    //task1AllDOne

--- app/src/main/java/com/example/myapplication_66051096/MainActivity.kt
+++ app/src/main/java/com/example/myapplication_66051096/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.ui.res.stringResource
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
@@ -16,7 +17,9 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -31,9 +34,10 @@ class MainActivity : ComponentActivity() {
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
+                    color = MaterialTheme.colorScheme.background,
+
                 ) {
-                    Greeting(message = "Happy Birthday Sam", from = "From Emma",modifier = Modifier.padding(8.dp))
+                    GreetingImage(message = stringResource(R.string.happy_birthday_text), from = stringResource(R.string.signature_text),modifier = Modifier.padding(8.dp))
                 }
             }
         }
@@ -49,14 +53,15 @@ fun Greeting(message: String,from:String, modifier: Modifier = Modifier) {
             fontSize = 100.sp,
             lineHeight = 116.sp,
             textAlign = TextAlign.Center,
-            modifier = modifier
+//            modifier = Modifier
+//                .padding(16.dp)
+//                .align(alignment = Alignment.End)
         )
         Text(text = from,
         fontSize = 36.sp,
-
-            modifier = Modifier
-                .padding(16.dp)
-                .align(alignment = Alignment.End)
+        modifier = Modifier
+            .padding(16.dp)
+            .align(alignment = Alignment.CenterHorizontally)
         )
     }
 
@@ -69,7 +74,9 @@ fun GreetingImage(message: String, from: String, modifier: Modifier = Modifier) 
     Box(modifier) {
         Image(
             painter = image,
-            contentDescription = null
+            contentDescription = null,
+            contentScale = ContentScale.Crop,
+            alpha = 0.5F
         )
         Greeting(
             message = message,
